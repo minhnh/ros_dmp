e20cf3456c4b4655cd485d739a4c908093e34d8a
Minh Nguyen <minhnh.91@gmail.com>
2019-06-08 13:55:47 +0200

    move traj & path creation to RollDmp class

--- scripts/generate_motion_service_node.py
+++ scripts/generate_motion_service_node.py
@@ -2,11 +2,11 @@
 import rospy
 import numpy as np
 from ros_dmp import RollDmp
-from ros_dmp.srv import *
-from ros_dmp.msg import *
+from ros_dmp.srv import GenerateMotion, GenerateMotionResponse
+from ros_dmp.msg import CartesianTrajectory
 from nav_msgs.msg import Path
-from geometry_msgs.msg import PoseStamped, Pose
 import tf
+
 
 class GenerateMotionClass:
 
@@ -23,69 +23,37 @@ class GenerateMotionClass:
 
         rospy.loginfo("Received motion generation request")
         # Initial pose
-        rpy = tf.transformations.euler_from_quaternion([req.initial_pose.pose.orientation.x, req.initial_pose.pose.orientation.y,
-                                                                    req.initial_pose.pose.orientation.z, req.initial_pose.pose.orientation.w])
-        initial_pose = np.array([req.initial_pose.pose.position.x, req.initial_pose.pose.position.y, req.initial_pose.pose.position.z,
-                            rpy[0], rpy[1], rpy[2]])
-        
+        rpy = tf.transformations.euler_from_quaternion([
+                req.initial_pose.pose.orientation.x, req.initial_pose.pose.orientation.y,
+                req.initial_pose.pose.orientation.z, req.initial_pose.pose.orientation.w])
+        initial_pose = np.array([
+                req.initial_pose.pose.position.x, req.initial_pose.pose.position.y, req.initial_pose.pose.position.z,
+                rpy[0], rpy[1], rpy[2]])
+
         # Goal Pose
-        rpy = tf.transformations.euler_from_quaternion([req.goal_pose.pose.orientation.x, req.goal_pose.pose.orientation.y,
-                                                                    req.goal_pose.pose.orientation.z, req.goal_pose.pose.orientation.w])
-        goal_pose = np.array([req.goal_pose.pose.position.x, req.goal_pose.pose.position.y, req.goal_pose.pose.position.z,
-                            rpy[0], rpy[1], rpy[2]])
+        rpy = tf.transformations.euler_from_quaternion([
+                req.goal_pose.pose.orientation.x, req.goal_pose.pose.orientation.y,
+                req.goal_pose.pose.orientation.z, req.goal_pose.pose.orientation.w])
+        goal_pose = np.array([
+                req.goal_pose.pose.position.x, req.goal_pose.pose.position.y, req.goal_pose.pose.position.z,
+                rpy[0], rpy[1], rpy[2]])
 
         rospy.loginfo("Generating motion for dmp " + req.dmp_name)
-        dmp = RollDmp(req.dmp_name, req.dt)
-        pos, vel, acc = dmp.roll(goal_pose, initial_pose, req.tau)
+        dmp = RollDmp(req.dmp_name, req.dt, "base_link")
 
         # Publish cartesian trajectory
-        cartesian_trajectory = CartesianTrajectory()
-        cartesian_trajectory.header.frame_id = "base_link"
-        path = Path()
-        path.header.frame_id = "base_link"
-        for i in range(pos.shape[0]):
-            x, y, z, w = tf.transformations.quaternion_from_euler(pos[i, 3], pos[i, 4], pos[i, 5])
-            pose = Pose()
-            pose.position.x = pos[i, 0]
-            pose.position.y = pos[i, 1]
-            pose.position.z = pos[i, 2]
-            pose.orientation.x = x
-            pose.orientation.y = y
-            pose.orientation.z = z
-            pose.orientation.w = w
-            
-            cartesian_state = CartesianState()
-            cartesian_state.pose = pose
-            pose_stamped = PoseStamped()
-            pose_stamped.pose = pose
-
-            cartesian_state.vel.linear.x = vel[i, 0]
-            cartesian_state.vel.linear.y = vel[i, 1]
-            cartesian_state.vel.linear.z = vel[i, 2]
-            cartesian_state.vel.angular.x = vel[i, 3]
-            cartesian_state.vel.angular.y = vel[i, 4]
-            cartesian_state.vel.angular.z = vel[i, 5]
-
-            cartesian_state.acc.linear.x = acc[i, 0]
-            cartesian_state.acc.linear.y = acc[i, 1]
-            cartesian_state.acc.linear.z = acc[i, 2]
-            cartesian_state.acc.angular.x = acc[i, 3]
-            cartesian_state.acc.angular.y = acc[i, 4]
-            cartesian_state.acc.angular.z = acc[i, 5]
-            
-            cartesian_trajectory.cartesian_state.append(cartesian_state)
-            path.poses.append(pose_stamped)
-        
+        cartesian_trajectory, path = dmp.get_trajectory_and_path(goal_pose, initial_pose, req.tau)
         self.trajectory_pub.publish(cartesian_trajectory)
         self.path_pub.publish(path)
+
+        # create and return service response
         response = GenerateMotionResponse()
         rospy.loginfo("Motion generated and published on respective toopics")
         response.result = "success"
         response.cart_traj = cartesian_trajectory
         return response
 
+
 if __name__ == "__main__":
-    
     obj = GenerateMotionClass()
     rospy.spin()
-
--- src/ros_dmp/roll_dmp.py
+++ src/ros_dmp/roll_dmp.py
@@ -1,22 +1,73 @@
 import numpy as np
 import yaml
-import os.path
+import os
 import pydmps
+import tf
+from geometry_msgs.msg import Pose, PoseStamped
+from nav_msgs.msg import Path
+from ros_dmp.msg import CartesianTrajectory, CartesianState
+
 
 class RollDmp():
 
-    def __init__(self, file_name, dt):
+    def __init__(self, weight_path, dt, target_frame):
+        if not os.path.exists(weight_path):
+            raise ValueError("weight file '{}' does not exist: ".format(weight_path))
 
-        weights = self.load_weights(file_name)
+        self.target_frame = target_frame
+
+        # create PyDMPs instance
+        weights = self.load_weights(weight_path)
         n_dmps = weights.shape[0]
         n_bfs = weights.shape[1]
         self.dmp = pydmps.dmp_discrete.DMPs_discrete(n_dmps=n_dmps, n_bfs=n_bfs,
                                                      dt=dt, ay=None, w=weights)
 
-    def roll(self, goal, initial_pos, tau):
-
-        self.pos, self.vel, self.acc = self.dmp.rollout(goal=goal, y0=initial_pos, tau=tau)
+    def roll(self, goal, initial_pose, tau):
+        self.pos, self.vel, self.acc = self.dmp.rollout(goal=goal, y0=initial_pose, tau=tau)
         return self.pos, self.vel, self.acc
+
+    def get_trajectory_and_path(self, goal_pose, initial_pose, tau):
+        pos, vel, acc = self.roll(goal_pose, initial_pose, tau)
+
+        cartesian_trajectory = CartesianTrajectory()
+        cartesian_trajectory.header.frame_id = self.target_frame
+        path = Path()
+        path.header.frame_id = self.target_frame
+        for i in range(pos.shape[0]):
+            x, y, z, w = tf.transformations.quaternion_from_euler(pos[i, 3], pos[i, 4], pos[i, 5])
+            pose = Pose()
+            pose.position.x = pos[i, 0]
+            pose.position.y = pos[i, 1]
+            pose.position.z = pos[i, 2]
+            pose.orientation.x = x
+            pose.orientation.y = y
+            pose.orientation.z = z
+            pose.orientation.w = w
+
+            cartesian_state = CartesianState()
+            cartesian_state.pose = pose
+            pose_stamped = PoseStamped()
+            pose_stamped.pose = pose
+
+            cartesian_state.vel.linear.x = vel[i, 0]
+            cartesian_state.vel.linear.y = vel[i, 1]
+            cartesian_state.vel.linear.z = vel[i, 2]
+            cartesian_state.vel.angular.x = vel[i, 3]
+            cartesian_state.vel.angular.y = vel[i, 4]
+            cartesian_state.vel.angular.z = vel[i, 5]
+
+            cartesian_state.acc.linear.x = acc[i, 0]
+            cartesian_state.acc.linear.y = acc[i, 1]
+            cartesian_state.acc.linear.z = acc[i, 2]
+            cartesian_state.acc.angular.x = acc[i, 3]
+            cartesian_state.acc.angular.y = acc[i, 4]
+            cartesian_state.acc.angular.z = acc[i, 5]
+
+            cartesian_trajectory.cartesian_state.append(cartesian_state)
+            path.poses.append(pose_stamped)
+
+        return cartesian_trajectory, path
 
     def load_weights(self, file_name):
 
@@ -37,4 +88,3 @@ class RollDmp():
         weights = np.vstack((weights, np.array(yaw)))
 
         return weights
-
